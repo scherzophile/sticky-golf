2ea5bab1e4f77fcd38468aa03119e891e6213e4e
Benjamin Ru <133164596+BenjaminHacks@users.noreply.github.com>
2025-10-17 17:24:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ forest = pygame.transform.scale(forest, (800, 600))
 play = pygame.image.load("img/play.png").convert_alpha()
 play = pygame.transform.scale(play, (100, 100))
 
-
 # MUSIC HERE
 pygame.mixer.init()
 pygame.mixer.music.load("audio/music.mp3")
@@ -32,6 +31,7 @@ pygame.mixer.music.play(loops=-1)
 running = True
 firing = False
 inair = False
+onground = False
 
 state = "title"
 
@@ -45,21 +45,54 @@ t = 0
 theta = 0
 new_dx = 0
 new_dy = 0
+platforms = [pygame.Rect(300,400,100,100),
+             pygame.Rect(0,510,800,90)]
+
+
+def checkplatform():
+    global vy, inair, y,onground, vx,x
+
+    ball_circle = pygame.Rect(x - 13, y - 13, 26, 26)
+
+
+    for platform in platforms:
+        if x < platform.left and ball_circle.colliderect(platform) and vx > 0:
+            x = platform.left - 13
+            vx = -vx * 0.5
+        elif x > platform.right and vx < 0 and ball_circle.colliderect(platform):
+            x = platform.right + 13
+            vx = -vx * 0.5
+        elif ball_circle.colliderect(platform) and vy > 0 and (y  > platform.y-5):
+            onground = True
+            y = platform.y - 5
+            vy *= -0.75
+            if abs(vy) < 1:
+                inair = False
+                vy = 0
+                vx = 0
+
+            break
+
+    if not onground:
+        inair = True
+
 
 while running:
-    screen.fill((0,0,0))
-    mx,my = pygame.mouse.get_pos()
+    screen.fill((0, 0, 0))
+    mx, my = pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             if state == "game" and event.button == 1:
                 firing = True
-            
+                onground = True
+
         if event.type == pygame.MOUSEBUTTONUP:
             if state == "game" and event.button == 1:
                 firing = False
                 inair = True
+                onground = False
             # Play button
             if state == "title" and event.button == 1:
                 if 350 <= mx and mx <= 450 and 400 <= my and my <= 500:
@@ -75,19 +108,14 @@ while running:
     if state == "game":
         if inair:
             vy -= a
-            if y + vy >= 500:
-                y = 500
-                # Try rebouncing (logic should apply for other objects too when they come in from top
-                if theta < 0.1:
-                    inair = False
-                else:
-                    theta /= 2
-                    vx /= 2
-                    vy *= -3/4
-            else:
-                x += vx
-                y += vy
+            x += vx
+            y += vy
+            onground = False
+            firing = False
 
+
+
+        checkplatform()
         # Background image yay
         screen.blit(bgimg, (0, 0))
 
@@ -97,41 +125,42 @@ while running:
         # here's the actual ball
         pygame.draw.circle(screen, (100, 100, 100), (x, y), 13)
         pygame.draw.circle(screen, (255, 255, 255), (x, y), 10)
+        for platform in platforms:
+            pygame.draw.rect(screen,(0,255,0),platform)
 
         if firing:
             viy = -15
             vy = viy
             if mx - x == 0:
-                theta = math.atan2(my-y, mx-x)
+                theta = math.atan2(my - y, mx - x)
             else:
                 theta = 1
             dy = my - y
             if dy == 0:
                 a = 0
             else:
-                a = -(vy)**2 / (2*dy)
+                a = -(vy) ** 2 / (2 * dy)
             t = 2 * dy / vy
             if t != 0:
-                vx = (mx-x)/t
+                vx = (mx - x) / t
             # now here's the trajectory of the ball
             # we need x and y in terms of t
             t_temp = 0
             new_dx = x
             new_dy = y
 
-            while abs(new_dx - 400) < 400 and a != 0 and mx-x != 0 and t != 0:
+            while abs(new_dx - 400) < 400 and a != 0 and mx - x != 0 and t != 0:
                 new_dx = x + vx * t_temp
-                new_dy = y + viy * t_temp - 0.5*a*t_temp**2
+                new_dy = y + viy * t_temp - 0.5 * a * t_temp ** 2
                 pygame.draw.circle(screen, (255, 0, 0), (new_dx, new_dy), 5)
                 t_temp += 2
-                print(new_dx, new_dy)
+
 
     pygame.display.flip()
     clock.tick(60)
 
 pygame.quit()
 sys.exit()
-
 
 """
 
